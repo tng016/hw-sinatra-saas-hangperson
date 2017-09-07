cbf4d3dc23937f9506eff5d776d1caae6cc6d2af
Ng Tze Yang <ngtzeyang94@gmail.com>
2017-09-07 00:12:26 +0000

    ready for heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,17 @@ class HangpersonGame
   # def initialize()
   # end
   
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
-
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
